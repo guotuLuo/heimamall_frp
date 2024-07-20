b40d42eaa08ae6ef8e16925872b7bfcf0ad48eef
guotuluo <2403105429@qq.com>
2024-07-20 16:37:39 +0800

    helloworld

---
+++ mp-demo/src/test/java/com/example/mpdemo/MpDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.mpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void hello(){
+        System.out.println("hello");
+    }
+}
